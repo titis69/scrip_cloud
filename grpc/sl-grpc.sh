c6b53b9c35d85bc43b37aec0119e04c45bece554
titis69 <117496052+titis69@users.noreply.github.com>
2024-06-17 14:27:28 +0700

    Update sl-grpc.sh

--- grpc/sl-grpc.sh
+++ grpc/sl-grpc.sh
@@ -6,7 +6,7 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 domain=$(cat /etc/xray/domain)
 apt install iptables iptables-persistent -y
-apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y
 apt install socat cron bash-completion ntpdate -y
 ntpdate pool.ntp.org
 apt -y install chrony
@@ -24,7 +24,7 @@ latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases |
 # / / Installation Xray Core
 xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
 
-# 
+#
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
 # / / Make Main Directory
@@ -41,107 +41,107 @@ chmod +x /usr/local/bin/xray
 # Make Folder XRay
 mkdir -p /var/log/xray/
 #
-wget -q -O /usr/local/bin/geosite.dat "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/geosite.dat"
-wget -q -O /usr/local/bin/geoip.dat "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/geoip.dat"
+wget -q -O /usr/local/bin/geosite.dat "https://raw.githubusercontent.com/titis69/scrip_cloud/main/grpc/geosite.dat"
+wget -q -O /usr/local/bin/geoip.dat "https://raw.githubusercontent.com/titis69/scrip_cloud/main/grpc/geoip.dat"
 
 #
 cat > /etc/xray/sl-vmessgrpc.json << END
 {
-    "log": {
-            "access": "/var/log/xray/access5.log",
-        "error": "/var/log/xray/error.log",
-        "loglevel": "info"
-    },
-    "inbounds": [
-        {
-            "port": 1180,
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "${uuid}"
+	"log": {
+        	"access": "/var/log/xray/access5.log",
+    	"error": "/var/log/xray/error.log",
+    	"loglevel": "info"
+	},
+	"inbounds": [
+    	{
+        	"port": 1180,
+        	"protocol": "vmess",
+        	"settings": {
+            	"clients": [
+                	{
+                    	"id": "${uuid}"
 #vmessgrpc
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "gun",
-                "security": "tls",
-                "tlsSettings": {
-                    "serverName": "${domain}",
-                    "alpn": [
-                        "h2"
-                    ],
-                    "certificates": [
-                        {
-                            "certificateFile": "/etc/xray/xray.crt",
-                            "keyFile": "/etc/xray/xray.key"
-                        }
-                    ]
-                },
-                "grpcSettings": {
-                    "serviceName": "GunService"
-                }
-            }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "tag": "direct"
-        }
-    ]
+                	}
+            	],
+            	"decryption": "none"
+        	},
+        	"streamSettings": {
+            	"network": "gun",
+            	"security": "tls",
+            	"tlsSettings": {
+                	"serverName": "${domain}",
+                	"alpn": [
+                    	"h2"
+                	],
+                	"certificates": [
+                    	{
+                        	"certificateFile": "/etc/xray/xray.crt",
+                        	"keyFile": "/etc/xray/xray.key"
+                    	}
+                	]
+            	},
+            	"grpcSettings": {
+                	"serviceName": "GunService"
+            	}
+        	}
+    	}
+	],
+	"outbounds": [
+    	{
+        	"protocol": "freedom",
+        	"tag": "direct"
+    	}
+	]
 }
 END
 
 cat > /etc/xray/sl-vlessgrpc.json << END
 {
-    "log": {
-            "access": "/var/log/xray/access5.log",
-        "error": "/var/log/xray/error.log",
-        "loglevel": "info"
-    },
-    "inbounds": [
-        {
-            "port": 2280,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "${uuid}"
+	"log": {
+        	"access": "/var/log/xray/access5.log",
+    	"error": "/var/log/xray/error.log",
+    	"loglevel": "info"
+	},
+	"inbounds": [
+    	{
+        	"port": 2280,
+        	"protocol": "vless",
+        	"settings": {
+            	"clients": [
+                	{
+                    	"id": "${uuid}"
 #vlessgrpc
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "gun",
-                "security": "tls",
-                "tlsSettings": {
-                    "serverName": "${domain}",
-                    "alpn": [
-                        "h2"
-                    ],
-                    "certificates": [
-                        {
-                            "certificateFile": "/etc/xray/xray.crt",
-                            "keyFile": "/etc/xray/xray.key"
-                        }
-                    ]
-                },
-                "grpcSettings": {
-                    "serviceName": "GunService"
-                }
-            }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "tag": "direct"
-        }
-    ]
+                	}
+            	],
+            	"decryption": "none"
+        	},
+        	"streamSettings": {
+            	"network": "gun",
+            	"security": "tls",
+            	"tlsSettings": {
+                	"serverName": "${domain}",
+                	"alpn": [
+                    	"h2"
+                	],
+                	"certificates": [
+                    	{
+                        	"certificateFile": "/etc/xray/xray.crt",
+                        	"keyFile": "/etc/xray/xray.key"
+                    	}
+                	]
+            	},
+            	"grpcSettings": {
+                	"serviceName": "GunService"
+            	}
+        	}
+    	}
+	],
+	"outbounds": [
+    	{
+        	"protocol": "freedom",
+        	"tag": "direct"
+    	}
+	]
 }
 END
 
@@ -194,10 +194,13 @@ systemctl restart sl-vless-grpc
 cd /usr/bin
 
 
-wget -O sl-addgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/sl-addgrpc.sh"
+wget -O sl-addgrpc "https://raw.githubusercontent.com/titis69/scrip_cloud/main/grpc/sl-addgrpc.sh"
 chmod +x sl-addgrpc
 chmod +x sl-delgrpc
 chmod +x sl-renewgrpc
 chmod +x sl-cekgrpc
 
 rm -f sl-grpc.sh
+
+
+
