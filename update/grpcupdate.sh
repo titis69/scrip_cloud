65d33faddb858785f9241778416df00b9ff4d19d
titis69 <117496052+titis69@users.noreply.github.com>
2024-06-17 15:03:55 +0700

    Update grpcupdate.sh

--- update/grpcupdate.sh
+++ update/grpcupdate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # ==========================================
 # ==========================================
-# 
+#
 cd
 
 cd /usr/bin
 
-wget -O addgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/addgrpc.sh"
-wget -O cekgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/cekgrpc.sh"
-wget -O delgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/delgrpc.sh"
-wget -O renewgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/renewgrpc.sh"
+wget -O addgrpc "https://raw.githubusercontent.com/titis69/scrip_cloud/main/xray/addgrpc.sh"
+wget -O cekgrpc "https://raw.githubusercontent.com/titis69/scrip_cloud/main/xray/cekgrpc.sh"
+wget -O delgrpc "https://raw.githubusercontent.com/titis69/scrip_cloud/main/xray/delgrpc.sh"
+wget -O renewgrpc "https://raw.githubusercontent.com/titis69/scrip_cloud/main/xray/renewgrpc.sh"
 
 
 chmod +x addgrpc
@@ -18,3 +18,6 @@ chmod +x cekgrpc
 chmod +x renewgrpc
 
 cd
+
+
+
