2cd73191ff4cde8e7fed63128bf1b65bb5385ca3
titis69 <117496052+titis69@users.noreply.github.com>
2024-06-17 14:24:51 +0700

    Update sl-download-info.sh

--- contohinfo/sl-download-info.sh
+++ contohinfo/sl-download-info.sh
@@ -9,17 +9,17 @@ rm info-exra-unli
 rm info-tsel-opok
 rm info-status-respon
 
-wget -O info-cek-tersedia "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/contohinfo/info-cek-tersedia.sh"
+wget -O info-cek-tersedia "https://raw.githubusercontent.com/titis69/scrip_cloud/main/contohinfo/info-cek-tersedia.sh"
 
-wget -O info-contoh-payload "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/contohinfo/info-contoh-payload.sh"
+wget -O info-contoh-payload "https://raw.githubusercontent.com/titis69/scrip_cloud/main/contohinfo/info-contoh-payload.sh"
 
-wget -O info-contohws "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/contohinfo/info-contohws.sh"
+wget -O info-contohws "https://raw.githubusercontent.com/titis69/scrip_cloud/main/contohinfo/info-contohws.sh"
 
-wget -O info-exra-unli "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/contohinfo/info-exra-unli.sh"
+wget -O info-exra-unli "https://raw.githubusercontent.com/titis69/scrip_cloud/main/contohinfo/info-exra-unli.sh"
 
-wget -O info-status-respon "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/contohinfo/info-status-respon.sh"
+wget -O info-status-respon "https://raw.githubusercontent.com/titis69/scrip_cloud/main/contohinfo/info-status-respon.sh"
 
-wget -O info-tsel-opok "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/contohinfo/info-tsel-opok.sh"
+wget -O info-tsel-opok "https://raw.githubusercontent.com/titis69/scrip_cloud/main/contohinfo/info-tsel-opok.sh"
 
 chmod +x info-cek-tersedia
 chmod +x info-contoh-payload
@@ -27,4 +27,7 @@ chmod +x info-contohws
 chmod +x info-exra-unli
 chmod +x info-tsel-opok
 chmod +x info-status-respon
+
+
+
 
