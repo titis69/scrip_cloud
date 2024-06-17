970195af51bfba740b530da791c1aa102b89ad61
titis69 <117496052+titis69@users.noreply.github.com>
2024-06-17 15:04:36 +0700

    Update grpcupdate2.sh

--- update/grpcupdate2.sh
+++ update/grpcupdate2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # ==========================================
 # ==========================================
-# 
+#
 cd
 
 cd /usr/bin
 
 
-wget -O fb-addgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/fb-addgrpc.sh"
-wget -O fb-delgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/fb-delgrpc.sh"
-wget -O fb-renewgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/fb-renewgrpc.sh"
-wget -O fb-cekgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/fb-cekgrpc.sh"
+wget -O fb-addgrpc "https://raw.githubusercontent.com/titis69/scrip_cloud/main/grpc/fb-addgrpc.sh"
+wget -O fb-delgrpc "https://raw.githubusercontent.com/titis69/scrip_cloud/main/grpc/fb-delgrpc.sh"
+wget -O fb-renewgrpc "https://raw.githubusercontent.com/titis69/scrip_cloud/main/grpc/fb-renewgrpc.sh"
+wget -O fb-cekgrpc "https://raw.githubusercontent.com/titis69/scrip_cloud/main/grpc/fb-cekgrpc.sh"
 
 chmod +x fb-addgrpc
 chmod +x fb-delgrpc
@@ -18,3 +18,6 @@ chmod +x fb-renewgrpc
 chmod +x fb-cekgrpc
 
 cd
+
+
+
